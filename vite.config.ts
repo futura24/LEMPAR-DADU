cb915f2a171d174dcac88f4e67520fd8d87801c3
futura24 <92496297+futura24@users.noreply.github.com>
2026-08-12 09:59:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
-// base './' membuat hasil build bisa dibuka dari sub-folder (GitHub Pages)
-// maupun dari root domain (Vercel/Netlify) tanpa perubahan konfigurasi.
 export default defineConfig({
-  base: '/LEMPAR DADU/',
+  base: '/LEMPAR-DADU/',
   plugins: [react()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  server: { port: 5173, open: true },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
